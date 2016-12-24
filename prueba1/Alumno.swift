cef9825ff138932ef8287a1fb1a344a6024aabeb
Juan Carlos <jcarlos@juans-imac.local>
2016-12-23 21:42:04 -0600

    funciona todo

--- prueba1/Alumno.swift
+++ prueba1/Alumno.swift
@@ -7,74 +7,58 @@
 //
 
 import Foundation
+import Eureka
+import JWT
 
-class AlumnoModel: NSObject {
+
+class Alumno {
     
     //properties
     
-    var nombre: String?
-    var ap: String?
-    var am: String?
-    var nombrec: String?
-    var estatus: String?
-    var carrera: String?
-    var nivel: Int?
-    var idc: Int?
+    let codigo: String
+    let nombre: String
+    let ap: String
+    let am: String
+    let nombrec: String
+    let estatus: String
+    let carrera: String
+    let nivel: String
+    let idc: String
+    let correo: String
+    let telefono: String
+    let fecha_nac: String
     
     
-    //empty constructor
     
-    override init()
-    {
-        
-    }
-    init(jsonStr:String) {
-        super.init()
-        
-        if let jsonData = jsonStr.data(using: String.Encoding.utf8, allowLossyConversion: false)
-        {
-            do {
-                let json = try JSONSerialization.jsonObject(with: jsonData, options: JSONSerialization.ReadingOptions.mutableContainers) as! Array<AnyObject>
-                
-                /* Loop
-                for (key, value) in json {
-                    let keyName = key as String
-                    let keyValue: String = value as! String
-                }
-                */
-            } catch let error as Error {
-                print("Failed to load: \(error.localizedDescription)")
+
+    init?(json: [String: Any]) {
+        guard let codigo = json["codigo"] as? String,
+            let nombre = json["nombre"] as? String,
+            let ap = json["ap"] as? String,
+            let am = json["am"] as? String,
+            let estatus = json["estatus"] as? String,
+            let carrera = json["carrera"] as? String,
+            let nivel = json["nivel"] as? String,
+            let clave = json["clave"] as? String,
+            let correo = json["correo"] as? String,
+            let telefono = json["telefono"] as? String,
+            let fecha_nac = json["fecha_nac"] as? String
+            else {
+                return nil
             }
-        }
-        else
-        {
-            print("json is of wrong format!")
-        }
-        // Or you can do it with using
-        // self.setValuesForKeysWithDictionary(JSONDictionary)
-        // instead of loop method above
-    }
-    //construct with @name, @address, @latitude, and @longitude parameters
-    
-    /*init(nombre: String, ap: String, am: String, estatus: String, carrera: String, nivel: Int, idc: Int) {
         
+        self.codigo = codigo
         self.nombre = nombre
         self.ap = ap
         self.am = am
-        self.nombrec = nombre + ap + am
+        self.nombrec = ap + am + nombre
         self.estatus = estatus
         self.carrera = carrera
+        self.idc = clave
         self.nivel = nivel
-        self.idc = idc
-    }*/
-    
-    
-    //prints object's current state
-    
-    override var description: String {
-        return "Nombre: \(nombre), estatus: \(estatus), carrera: \(carrera), idc: \(idc)"
-        
+        self.correo = correo
+        self.telefono = telefono
+        self.fecha_nac = fecha_nac
     }
-    
     
 }
--- prueba1/DatosViewController.swift
+++ prueba1/DatosViewController.swift
@@ -19,84 +19,23 @@ class DatosViewController: FormViewController {
     @IBOutlet weak var txtFechaNac: UITextField!
     @IBOutlet var txtCarrera: UIView!
     
+
     
-    
-    let datos = AlumnoModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-    
-        
-        form = Section("Datos")
-            <<< LabelRow(){ row in
-                row.title = "Código"
-                row.value = ""
-        }
-            <<< LabelRow(){ row in
-                row.title = "Nombre"
-                row.value = "Ingresa texto aqui"
-            }
-            
-            <<< EmailRow(){
-                $0.title = "Correo Electrónico"
-                $0.placeholder = "Introduce tu Email"
-            }
-            <<< PhoneRow(){
-                $0.title = "Telefono"
-                $0.placeholder = "Ingresa tu numero aquí"
-        }
-            <<< DateRow(){
-                $0.title = "Fecha de Nacimiento"
-                $0.value = NSDate(timeIntervalSinceReferenceDate: 0) as Date
-            }
-    +++ Section("Datos Académicos")
-            <<< LabelRow(){
-                $0.title = "Carrera"
-                $0.value = "LIC TURISMO (TURA)"
-        }
         
         
         let payload = try JWT.encode(["token": "CONTROL-ESCOLAR-CUCEA","codigo": "216355666"], algorithm: .hs256("secret".data(using: .utf8)!))
         
         
-        
-        do {
-            let payload2 = try JWT.decode(payload, algorithm: .hs256("secret".data(using: .utf8)!))
-            //print(payload2)
-        } catch {
-            print("Failed to decode JWT: \(error)")
-        }
-            
-
-        
         let urlPath: String = "http://cescolar.cucea.udg.mx/servicios/service.php?token=\(payload)" //this will be changed to the path where service.php lives
-        //print(urlPath)
+        print(urlPath)
         let url = URL(string: urlPath)
         
         let theRequest = NSMutableURLRequest(url: url! as URL)
         
-        let configuration = URLSessionConfiguration.default
-        
         let session = URLSession.shared
-        /*let task = session.dataTask(with: theRequest as URLRequest, completionHandler: {data, response, error -> Void in
-            guard let responseData = data else {
-                print("Error: did not receive data")
-                return
-            }
-            // parse the result as JSON, since that's what the API provides
-            do {
-                guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
-                    as? [String: Any] else {
-                        print("error trying to convert data to JSON")
-                        return
-                }
-                // now we have the todo
-                // let's just print it to prove we can access it
-                print("The todo is: " + todo.description)
-            //let alumno = AlumnoModel(jsonStr: response)
-            //print(alumno.name!) // Output is "myUser"
-        })*/
             
             let task = session.dataTask(with: theRequest as URLRequest) {
                 (data, response, error) in
@@ -111,16 +50,34 @@ class DatosViewController: FormViewController {
                     print("Error: did not receive data")
                     return
                 }
-                // parse the result as JSON, since that's what the API provides
                 do {
-                    let responseData = String(data: data!, encoding: String.Encoding.utf8)
-                    //print(responseData)
+                    guard let todo = try JSONSerialization.jsonObject(with: responseData, options: [])
+                        as? [String: Any] else {
+                            print("error trying to convert data to JSON")
+                            return
+                    }
                     
-                    let jsonData = responseData?.data(using: String.Encoding.utf8, allowLossyConversion: true)
+                    guard let alumno = Alumno(json: todo)
+                        else {
+                            print("error construyendo objeto")
+                            return
+                    }
+                    
+                    let datos = UserDefaults.standard
+                    
+                    datos.setValue(alumno.codigo, forKey: "codigo")
+                    datos.setValue(alumno.nombre, forKey: "nombre")
+                    datos.setValue(alumno.ap, forKey: "ap")
+                    datos.setValue(alumno.am, forKey: "am")
+                    datos.setValue(alumno.carrera, forKey: "carrera")
+                    datos.setValue(alumno.estatus, forKey: "estatus")
+                    datos.setValue(alumno.idc, forKey: "clave")
+                    datos.setValue(alumno.correo, forKey: "correo")
+                    datos.setValue(alumno.telefono, forKey: "telefono")
+                    datos.setValue(alumno.fecha_nac, forKey: "fecha_nac")
                     
                     
-                    let json = try JSONSerialization.jsonObject(with: jsonData!, options: JSONSerialization.ReadingOptions.mutableContainers) as! Array<AnyObject>
-                    print(json[0]["nombre"])
+                
                     
                 } catch  {
                     print("error trying to convert data to JSON")
@@ -129,7 +86,37 @@ class DatosViewController: FormViewController {
             }
         
         task.resume()
-    
+        
+        let datos = UserDefaults.standard
+        print(datos)
+
+        form = Section("Datos")
+            <<< LabelRow(){ row in
+                row.title = "Código"
+                row.value = datos.value(forKey: "codigo") as! String?
+            }
+            <<< LabelRow(){ row in
+                row.title = "Nombre"
+                row.value = datos.value(forKey: "nombre") as! String?
+            }
+            
+            <<< EmailRow(){
+                $0.title = "Correo Electrónico"
+                $0.placeholder = datos.value(forKey: "correo") as! String?
+            }
+            <<< PhoneRow(){
+                $0.title = "Telefono"
+                $0.placeholder = datos.value(forKey: "telefono") as! String?
+            }
+            <<< LabelRow(){
+                $0.title = "Fecha de Nacimiento"
+                $0.value = datos.value(forKey: "fecha_nac") as! String?
+            }
+            +++ Section("Datos Académicos")
+            <<< LabelRow(){
+                $0.title = "Carrera"
+                $0.value = UserDefaults.standard.value(forKey: "carrera") as! String?
+        }
         
 
         // Do any additional setup after loading the view.
